345dd6002175a4a856e1e6a3b5bef50717cd93b8
xgon3l <s29784@pjwstk.edu.pl>
2026-03-23 18:59:44 +0100

    feat: add Equipment abstract class

---
+++ WypozyczalniaSprzetu/Models/Equipments/Equipment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaSprzetu.Models.Equipments
+{
+    public abstract class Equipment
+    {
+        private static int _nextId = 1;
+        public int Id { get; }
+        public String Name { get; set; }
+        public EquipmentStatus Status { get; set; }
+
+        public Equipment(string name)
+        {
+            Id = _nextId++;
+            Name = name;
+        }
+    }
+}
