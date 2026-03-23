c64872100f75a9f73467969dea5304e39ad2afe6
xgon3l <s29784@pjwstk.edu.pl>
2026-03-23 22:28:45 +0100

    feat: add InMemoryStore

---
+++ WypozyczalniaSprzetu/Services/InMemoryStore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using WypozyczalniaSprzetu.Models;
+using WypozyczalniaSprzetu.Models.Equipments;
+using WypozyczalniaSprzetu.Models.Users;
+
+public class InMemoryStore
+{
+    public List<User> Users { get; } = new List<User>();
+    public List<Equipment> Equipments { get; } = new List<Equipment>();
+    public List<Rental> Rentals { get; } = new List<Rental>();
+}
