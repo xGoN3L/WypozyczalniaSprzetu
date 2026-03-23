842427c0c5aa03260cf9fc18e75a64fbf5ff98aa
xgon3l <s29784@pjwstk.edu.pl>
2026-03-23 19:01:43 +0100

    feat: add models that inherits from Equipment abstract class

---
+++ WypozyczalniaSprzetu/Models/Equipments/Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaSprzetu.Models.Equipments
+{
+    public class Laptop : Equipment
+    {
+        public string ScreenSize { get; set; }
+        public string Processor { get; set; }
+        public bool HasDedicatedGraphicsCard { get; set; }
+        public string GraphicsCardModel { get; set; }
+        public int RAM { get; set; }
+        public Laptop(string name) : base(name)
+        {
+        }
+    }
+}
---
+++ WypozyczalniaSprzetu/Models/Equipments/Projector.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaSprzetu.Models.Equipments
+{
+    public class Projector : Equipment
+    {
+        public string Resolution { get; set; }
+        public int Lumens { get; set; }
+        public bool HasHDMI { get; set; }
+        public bool HasVGA { get; set; }
+        public Projector(string name) : base(name)
+        {
+        }
+    }
+}
