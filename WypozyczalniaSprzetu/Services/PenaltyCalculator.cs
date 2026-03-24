85f4f5e2d26c1d4fd62e71d659e342582509a735
xgon3l <s29784@pjwstk.edu.pl>
2026-03-24 22:38:14 +0100

    feat: add PenaltyCalculator with CalculatePenalty method

---
+++ WypozyczalniaSprzetu/Services/PenaltyCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WypozyczalniaSprzetu.Services
+{
+    public class PenaltyCalculator
+    {
+        private const decimal DailyPenaltyRate = 5.0m;
+
+        public decimal CalculatePenalty(DateTime rentReturnDate)
+        {
+            if (DateTime.Now <= rentReturnDate)
+            {
+                return 0;
+            }
+            int daysLate = (DateTime.Now - rentReturnDate).Days;
+            return daysLate * DailyPenaltyRate;
+        }
+    }
+}
