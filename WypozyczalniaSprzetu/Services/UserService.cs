ea0a89417d1883139603520ca1cc864366f74ca7
xgon3l <s29784@pjwstk.edu.pl>
2026-03-24 22:38:44 +0100

    feat: add UserService with AddUser method and constructor

---
+++ WypozyczalniaSprzetu/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WypozyczalniaSprzetu.Models.Users;
+
+namespace WypozyczalniaSprzetu.Services
+{
+    public class UserService
+    {
+        private readonly InMemoryStore _store;
+        public UserService(InMemoryStore store)
+        {
+            _store = store;
+        }
+        public void AddUser(User user)
+        {
+            _store.Users.Add(user);
+        }
+    }
+}
