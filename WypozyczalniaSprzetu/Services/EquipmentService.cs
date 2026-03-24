dafd58c5407e94c9ff6ddd033f636d17385824c7
xgon3l <s29784@pjwstk.edu.pl>
2026-03-24 22:37:52 +0100

    feat: add EquipmentService with constructor and AddEquipment, GetAllEquipments method

---
+++ WypozyczalniaSprzetu/Services/EquipmentService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WypozyczalniaSprzetu.Models.Equipments;
+
+namespace WypozyczalniaSprzetu.Services
+{
+    public class EquipmentService
+    {
+        private readonly InMemoryStore _store;
+        public EquipmentService(InMemoryStore store)
+        {
+            _store = store;
+        }
+        public void AddEquipment(Equipment equipment)
+        {
+            _store.Equipments.Add(equipment);
+        }
+        public List<Equipment> GetAllEquipments()
+        {
+            return _store.Equipments;
+        }
+    }
+}
