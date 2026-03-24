704762c8196554265bcce3fa75093464e7e14443
xgon3l <s29784@pjwstk.edu.pl>
2026-03-24 22:34:40 +0100

    feat: add Rentals folder and extend Rental class with nextId static property and add Rental constructor

--- WypozyczalniaSprzetu/Models/Rentals/Rental.cs
+++ WypozyczalniaSprzetu/Models/Rentals/Rental.cs
@@ -6,18 +6,26 @@ using System.Threading.Tasks;
 using WypozyczalniaSprzetu.Models.Equipments;
 using WypozyczalniaSprzetu.Models.Users;
 
-namespace WypozyczalniaSprzetu.Models
+namespace WypozyczalniaSprzetu.Models.Rentals
 {
     public class Rental
     {
+        private static int _nextId = 1;
         public int Id { get; }
         public User Borrower { get; set; }
         public Equipment EquipmentRented { get; set; }
         public DateTime RentDateStart { get; set; }
-        public DateTime RentReturnDate { get; set; }
+        public DateTime? RentReturnDate { get; set; }
         public bool IsReturnedInTime => DateTime.Now <= RentReturnDate;
         public decimal RentPrice { get; set; }
         public decimal? RentFine { get; set; }
-
+        public Rental(User borrower, Equipment equipment, DateTime rentDateStart, decimal rentPrice)
+        {
+            Id = _nextId++;
+            Borrower = borrower;
+            EquipmentRented = equipment;
+            RentDateStart = rentDateStart;
+            RentPrice = rentPrice;
+        }
     }
 }
