9e80fcbd7d598740b42c71ea8c75b1fe80370649
xgon3l <s29784@pjwstk.edu.pl>
2026-03-24 22:35:28 +0100

    feat: add Data folder and move InMemoryStore class to the Data folder

---
+++ WypozyczalniaSprzetu/Data/InMemoryStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using WypozyczalniaSprzetu.Models;
+using WypozyczalniaSprzetu.Models.Equipments;
+using WypozyczalniaSprzetu.Models.Users;
+
+namespace WypozyczalniaSprzetu.Services
+{
+    public class InMemoryStore
+    {
+        public List<User> Users { get; } = new List<User>();
+        public List<Equipment> Equipments { get; } = new List<Equipment>();
+        public List<Rental> Rentals { get; } = new List<Rental>();
+    }
+}
