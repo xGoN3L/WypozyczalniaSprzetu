21c1551db20fbbf4692511deb247fdcb2f3c0d34
xgon3l <s29784@pjwstk.edu.pl>
2026-03-25 23:15:32 +0100

    feat: adjust namespace for InMemoryStore class

--- WypozyczalniaSprzetu/Data/InMemoryStore.cs
+++ WypozyczalniaSprzetu/Data/InMemoryStore.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using WypozyczalniaSprzetu.Models;
 using WypozyczalniaSprzetu.Models.Equipments;
+using WypozyczalniaSprzetu.Models.Rentals;
 using WypozyczalniaSprzetu.Models.Users;
 
-namespace WypozyczalniaSprzetu.Services
+namespace WypozyczalniaSprzetu.Data
 {
     public class InMemoryStore
     {
