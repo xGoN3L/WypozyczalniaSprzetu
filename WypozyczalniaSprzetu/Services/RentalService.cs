997c6d380405e547973d9833904a551d66347a1a
xgon3l <s29784@pjwstk.edu.pl>
2026-03-25 23:24:32 +0100

    feat: add GetRentalForEquipment, ReturnEquipment, ShowAvailableRentalsForUser, ShowRentalsNotReturnedInTime method to RentalService class

--- WypozyczalniaSprzetu/Services/RentalService.cs
+++ WypozyczalniaSprzetu/Services/RentalService.cs
@@ -26,9 +26,9 @@ namespace WypozyczalniaSprzetu.Services
             var equipment = _store.Equipments.FirstOrDefault(e => e.Id == equipmentId);
             if (user == null || equipment == null)
             {
-                throw new Exception("Użytkownik lub sprzęt nie został znaleziony");
+                Console.WriteLine("Użytkownik lub sprzęt nie został znaleziony");
             }
-            int activeRentals = _store.Rentals.Count(r => r.UserId == userId && r.ReturnDate == null);
+            int activeRentals = _store.Rentals.Count(r => r.Borrower.Id == userId && r.RentReturnDate == null);
             if (activeRentals >= _borrowingPolicy.GetMaxActiveRentals(user))
             {
                 Console.WriteLine("Użytkownik osiągnął limit wypożyczeń");
@@ -42,9 +42,65 @@ namespace WypozyczalniaSprzetu.Services
             _store.Rentals.Add(new Rental(user, equipment, DateTime.Now, rentReturnEndDate, rentPrice));
             equipment.Status = EquipmentStatus.Borrowed;
         }
+        public Rental GetRentalForEquipment(int equipmentId)
+        {
+            var rental = _store.Rentals.FirstOrDefault(r => r.EquipmentRented.Id == equipmentId && r.RentReturnDate == null);
+            if (rental == null)
+            {
+                Console.WriteLine("Sprzęt nie jest aktualnie wypożyczony");
+                return null;
+                //TODO
             }
-            _store.Rentals.Add(new Rental(user, equipment, DateTime.Now, rentPrice));
-            equipment.Status = EquipmentStatus.Rented;
+            else
+            {
+                return rental;
+            }
+        }
+        public void ReturnEquipment(int rentalId)
+        {
+            var rental = _store.Rentals.FirstOrDefault(r => r.Id == rentalId);
+            if (rental == null || rental.RentReturnDate != null)
+            {
+                Console.WriteLine("Nieprawidłowy identyfikator wypożyczenia lub sprzęt już został zwrócony");
+                return;
+            }
+            rental.RentReturnDate = DateTime.Now;
+            rental.EquipmentRented.Status = EquipmentStatus.Available;
+            decimal penalty = _penaltyCalculator.CalculatePenalty(rental.RentReturnEndDate, DateTime.Now);
+            if (penalty > 0)
+            {
+                Console.WriteLine($"Należna kara za spóźnienie: {penalty}");
+            }
+            rental.RentFine = penalty;
+        }
+        public void ShowAvailableRentalsForUser(int userId)
+        {
+            var rentals = _store.Rentals.Where(x => x.Borrower.Id == userId && x.RentReturnDate == null).ToList();
+            var user = _store.Users.FirstOrDefault(u => u.Id == userId);
+            if (rentals.Count == 0)
+            {
+                Console.WriteLine("Brak aktywnych wypożyczeń dla tego użytkownika.");
+            }
+            else
+            {
+                Console.WriteLine($"Aktywne wypożyczenia dla użytkownika {user.FirstName} {user.LastName}:");
+                rentals.ForEach(x => Console.WriteLine(x));
+                Console.WriteLine("____________________");
+            }
+        }
+        public void ShowRentalsNotReturnedInTime()
+        {
+            var rentals = _store.Rentals.Where(x => x.RentReturnDate == null && x.RentReturnEndDate < DateTime.Now).ToList();
+            if (rentals.Count == 0)
+            {
+                Console.WriteLine("Brak wypożyczeń, które nie zostały zwrócone w terminie.");
+            }
+            else
+            {
+                Console.WriteLine("Wypożyczenia, które nie zostały zwrócone w terminie:");
+                rentals.ForEach(x => Console.WriteLine(x));
+                Console.WriteLine("____________________");
+            }
         }
     }
 }
