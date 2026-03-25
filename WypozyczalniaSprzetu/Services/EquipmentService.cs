1dac16f108463cceda54695486f4ecad885bc94f
xgon3l <s29784@pjwstk.edu.pl>
2026-03-25 23:07:10 +0100

    feat: extend EquipmentService with ShowAllEquipment, ShowAvailableEquipment, SetEquipmentAsUnavailable methods

--- WypozyczalniaSprzetu/Services/EquipmentService.cs
+++ WypozyczalniaSprzetu/Services/EquipmentService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WypozyczalniaSprzetu.Data;
 using WypozyczalniaSprzetu.Models.Equipments;
 
 namespace WypozyczalniaSprzetu.Services
@@ -18,9 +19,26 @@ namespace WypozyczalniaSprzetu.Services
         {
             _store.Equipments.Add(equipment);
         }
-        public List<Equipment> GetAllEquipments()
+        public void ShowAllEquipment()
         {
-            return _store.Equipments;
+            Console.WriteLine("Cały sprzęt: ");
+            _store.Equipments.ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("_______________");
+        }
+        public void ShowAvailableEquipment()
+        {
+            Console.WriteLine("Dostępny sprzęt do wypożyczenia: ");
+            _store.Equipments.Where(x => x.Status == EquipmentStatus.Available).ToList().ForEach(x => Console.WriteLine(x));
+            Console.WriteLine("_______________");
+        }
+        public void SetEquipmentAsUnavailable(int equipmentId)
+        {
+            var equipment = _store.Equipments.FirstOrDefault(e => e.Id == equipmentId);
+            if (equipment == null)
+            {
+                throw new Exception("Sprzęt nie został znaleziony");
+            }
+            equipment.Status = EquipmentStatus.Unvailable;
         }
     }
 }
